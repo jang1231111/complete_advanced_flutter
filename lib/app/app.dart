5e78a9eccf0502d107a48e56544cd1a09c31b317
JYC <uiopzx9782@naver.com>
2024-03-06 15:21:43 +0900

    Lecture 21 - Adding Splah Timer

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,5 @@
+import 'package:completed_advanced_flutter/presentation/resources/routes_manager.dart';
+import 'package:completed_advanced_flutter/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -16,7 +18,10 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
+      theme: getApplicationTheme(),
+    );
   }
-  
 }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:completed_advanced_flutter/presentation/resources/assets_manager.dart';
+import 'package:completed_advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:completed_advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -8,8 +13,35 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(Duration(seconds: 2), () {
+      _goNext();
+    });
+  }
+
+  _goNext() {
+    Navigator.pushNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: Center(child: Image(image: AssetImage(ImageAssets.splashLogo))),
+    );
   }
 }
